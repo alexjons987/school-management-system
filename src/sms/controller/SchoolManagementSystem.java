e1b2bd6e5718079c72d4233ab371e2776a396dff
alexjons987 <alexander.jonsson987@outlook.com>
2025-10-07 16:25:10 +0200

    Add mock data, Update main program loop

--- src/sms/controller/SchoolManagementSystem.java
+++ src/sms/controller/SchoolManagementSystem.java
@@ -8,6 +8,7 @@ import sms.view.SchoolManagementSystemUI;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class SchoolManagementSystem {
 
@@ -29,19 +30,68 @@ public class SchoolManagementSystem {
     private void initializeSMS() throws IOException {
         try {
             // TODO: Read previously saved data from JSON database
+
+            // TODO: Remove test data (ONLY USED FOR TESTING PRE-DATABASE)
+            // Create some students
+            students = List.of(
+                    new Student("Alexander", "A", 1234, "alex@email.com", "0721234567"),
+                    new Student("Hans-Erik", "HE", 2345, "hanserik@email.com", "0722345678"),
+                    new Student("Lena", "L", 3456, "lena@email.com", "0723456789"),
+                    new Student("Ioana", "I", 4567, "ioana@email.com", "0724567890")
+            );
+
+            // Create some teachers
+            teachers = List.of(
+                    new Teacher("Alice", "A.", 1234, "alex@email.com", "0721234567"),
+                    new Teacher("Bob", "B.", 2345, "hanserik@email.com", "0722345678"),
+                    new Teacher("Charlie", "C.", 3456, "lena@email.com", "0723456789")
+            );
+
+            // Create some courses
+            courses = List.of(
+                    new Course("Algebra and Geometry (IX1303)", "Math", teachers.get(0)),
+                    new Course("Computer Hardware Engineering (IS1200)", "Engineering", teachers.get(1)),
+                    new Course("Data Storage Paradigms (IV1351)", "Engineering", teachers.get(2))
+            );
+
+            // Add a course manager to each course
+            teachers.get(0).addCourse(courses.get(0));
+            teachers.get(1).addCourse(courses.get(1));
+            teachers.get(2).addCourse(courses.get(2));
+
+            // Add students to courses
+            for (Student student : students) {
+                for (Course course : courses) {
+                    student.addCourse(course);
+                }
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
             throw new IOException();
         }
-
     }
 
     private void mainSMS() {
-        boolean flag = true;
-        SchoolManagementSystemUI.printTitle();
-        while (flag) {
-            flag = SchoolManagementSystemUI.startMenu();
-        }
+        Scanner scanner = new Scanner(System.in);
+        int menuChoice;
+
+        do {
+            SchoolManagementSystemUI.printTitle();
+            menuChoice = SchoolManagementSystemUI.startMenu(scanner);
+
+            switch (menuChoice) {
+                case 1:
+                    SchoolManagementSystemUI.manageStudentsMenu(scanner, students);
+                    break;
+                case 2:
+                    SchoolManagementSystemUI.manageTeachersMenu(scanner);
+                    break;
+                case 3:
+                    SchoolManagementSystemUI.manageCoursesMenu(scanner);
+                    break;
+            }
+        } while (menuChoice != 0);
         System.out.println("Goodbye!");
         // TODO: Save data to JSON database
     }
