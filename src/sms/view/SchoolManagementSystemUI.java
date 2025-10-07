4bc6ae276dbe89c56d4d4a63685ad64ee010b650
alexjons987 <alexander.jonsson987@outlook.com>
2025-10-07 11:40:22 +0200

    Add sub menu methods

--- src/sms/view/SchoolManagementSystemUI.java
+++ src/sms/view/SchoolManagementSystemUI.java
@@ -9,7 +9,43 @@ public class SchoolManagementSystemUI {
     }
 
     public static void startMenu() {
-        readIntMenuChoice(new Scanner(System.in), 0, 4);
+        // 1. Manage students
+        // 2. Manage teachers
+        // 3. Manage courses
+        readIntMenuChoice(new Scanner(System.in), 1, 3);
+    }
+
+    public static void manageStudentsMenu() {
+        /**
+         *     1. Add student
+         *     2. Remove student
+         *     3. Grade student
+         *     4. List students
+         *     5. List grades of student -> select student
+         */
+        readIntMenuChoice(new Scanner(System.in), 1, 5);
+    }
+
+    public static void manageTeachersMenu() {
+        /**
+         *     1. Add Teacher
+         *     2. Remove Teacher
+         *     3. List Teachers
+         *     4. List courses of teacher -> select teacher
+         */
+        readIntMenuChoice(new Scanner(System.in), 1, 4);
+    }
+
+    public static void manageCoursesMenu() {
+        /**
+         *     1. Add course
+         *     2. Remove course
+         *     3. Add student to course
+         *     4. List all courses and their course manager
+         *     5. Assign course to teacher -> warn if you are replacing current teacher
+         *     6. See course details -> select course -> list course name, subject, course manager, students
+         */
+        readIntMenuChoice(new Scanner(System.in), 1, 6);
     }
 
     /**
